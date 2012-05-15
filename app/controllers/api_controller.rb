9ab801b488f840858f5247802d240f401bd8ca59
Matt Yoho <mby@mattyoho.com>
2012-05-15 10:19:21 -0400

    API basic auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,26 @@
 class ApiController < ActionController::Base
+  before_filter :authenticate
 
   private
+
+  def authenticate
+    if self.current_user = authenticate_with_http_basic do |username, password|
+        login(username, password)
+      end
+    else
+      request_http_basic_authentication
+    end
+  end
 
   def construct_link_header(next_url, last_url)
     "<#{next_url}>; rel=\"next\", <#{last_url}>; rel=\"last\""
   end
 
+  def current_user
+    @current_user
+  end
+
+  def current_user=(user)
+    @current_user = user
+  end
 end
